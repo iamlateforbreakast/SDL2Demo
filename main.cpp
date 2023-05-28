0eaa6125acb39265497a5e20afae31243c73fd88
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2023-05-28 23:12:17 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include "Game.h"
+
+int main(int argc, char * argv[])
+{
+  Game game;
+  
+  game.start();
+  
+  while (game.isRunning())
+  {
+    game.readUserInputs();
+    game.wait();
+  }
+  
+  game.complete();
+}
