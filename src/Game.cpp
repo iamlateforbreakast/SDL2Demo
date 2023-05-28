55e4608da9a92380e200ed246321dc1932f8784f
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2023-05-28 23:58:53 +0200

    Create Game.cpp

---
+++ src/Game.cpp
@@ -0,0 +1,45 @@
+#include "Game.h"
+
+Game::Game()
+{
+}
+
+Game::~Game()
+{
+}
+
+void Game::start()
+{
+}
+  
+bool Game::isRunning()
+{
+  return FALSE;
+}
+
+void Game::readUserInput()
+{
+}
+
+void Game::complete()
+{
+}
+
+void Game::initSDL()
+{
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+
+}
